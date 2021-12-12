ec2009ee482173e3680c78871d9fc88e632cca60
Steven Barquet <mlmjm123@gmail.com>
2021-12-12 10:39:34 -0600

    true jalador

--- src/javascript/Routes.tsx
+++ src/javascript/Routes.tsx
@@ -1,6 +1,17 @@
+/* eslint-disable react/jsx-fragments */
 // ---Dependencys
-import React, {ReactElement} from 'react';
+import { ReactElement, Fragment, useEffect } from 'react';
 import { Route, Switch, withRouter, RouteComponentProps } from 'react-router-dom';
+// ---Redux
+import { useDispatch } from 'react-redux';
+import {
+  changeResponsiveFlag,
+  updatePath,
+  updateParams
+} from '@Redux/appInfo/actions';
+import { ResponsiveData } from '@Redux/appInfo/customTypes';
+// ---Others
+import isMovilDetector from 'Others/isMovilDetector';
 // ---Pages
 import HomePage from 'Pages/HomePage';
 import Error404Page from 'Pages/Error404Page';
@@ -8,16 +19,29 @@ import Error404Page from 'Pages/Error404Page';
 function Routes(props: RouteComponentProps): ReactElement {
   const { pathname, search: urlParams } = props.location;
   const currentPath = pathname
-  console.log('Router pathname: ', pathname);
-  console.log('Router urlParams: ', urlParams);
-  
+  const responsiveData = isMovilDetector();
+  // Redux Actions
+  const dispatch = useDispatch();
+  const updateResponsiveData = (data: ResponsiveData) => dispatch(changeResponsiveFlag(data));
+  const updateCurrentPath = () => dispatch(updatePath(currentPath));
+  const updateCurrentParams = () => dispatch(updateParams(urlParams));  
+  useEffect(() => {
+    updateResponsiveData(responsiveData);
+  }, [responsiveData]);
+
+  useEffect(() => { updateCurrentPath() }, [currentPath]);
+  useEffect(() => { updateCurrentParams() }, [urlParams]);
   return(
-    <>
+    <Fragment>
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="*" component={Error404Page} />
+        <Route path="/">
+          <HomePage />
+        </Route>
+        <Route path="*">
+          <Error404Page />
+        </Route>
       </Switch>
-    </>
+    </Fragment>
   )
 }
 
